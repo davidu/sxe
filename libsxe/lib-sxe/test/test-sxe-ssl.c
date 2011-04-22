9f6fc12bc0fa61d6733380b870aedf238d48090f
Neil Watkiss <neil.watkiss@gmail.com>
2011-04-22 15:59:38 -0700

    Clean up indentation. W/S and comments only.

--- libsxe/lib-sxe/test/test-sxe-ssl.c
+++ libsxe/lib-sxe/test/test-sxe-ssl.c
@@ -90,8 +90,8 @@ main(void)
     plan_tests(20);
     sxe_register(6, 0);
     sxe_ssl_register(2);
-    is(sxe_init(), SXE_RETURN_OK,                                                        "sxe_init succeeded");
-    is(sxe_ssl_init(cert, pkey, cert, "."), SXE_RETURN_OK,                               "sxe_ssl_init succeeded");
+    is(sxe_init(), SXE_RETURN_OK,                                                           "sxe_init succeeded");
+    is(sxe_ssl_init(cert, pkey, cert, "."), SXE_RETURN_OK,                                  "sxe_ssl_init succeeded");
 
     listener = sxe_new_tcp(NULL, "0.0.0.0", 0, test_event_connected, test_event_read, test_event_close);
     client   = sxe_new_tcp(NULL, "0.0.0.0", 0, test_event_connected, test_event_read, test_event_close);
@@ -102,50 +102,79 @@ main(void)
     /* Set up the *listener* as an SSL server. The listener doesn't actually
      * do any SSL stuff, but this setting causes accepted sockets to
      * automatically begin speaking SSL. */
-    is(sxe_ssl_enable(listener), SXE_RETURN_OK, "sxe_ssl_enable succeeded");
-    SXEA10(sxe_listen(listener) == SXE_RETURN_OK, "Failed to sxe_listen()");
+    {
+        is(sxe_ssl_enable(listener), SXE_RETURN_OK,                                         "sxe_ssl_enable succeeded");
+        SXEA10(sxe_listen(listener) == SXE_RETURN_OK,                                       "Failed to sxe_listen()");
+    }
 
-    /* Set up the client as an SSL client. */
-    is(sxe_ssl_enable(client), SXE_RETURN_OK, "sxe_ssl_enable succeeded");
-    SXEA10(sxe_connect(client, "127.0.0.1", SXE_LOCAL_PORT(listener)) == SXE_RETURN_OK, "Failed to sxe_connect()");
+    /* Set up the client as an SSL client, and connect to the server. Ensure
+     * we get two connected events: one for the client and one for the server.
+     */
+    {
+        is(sxe_ssl_enable(client), SXE_RETURN_OK, "sxe_ssl_enable succeeded");
+        SXEA10(sxe_connect(client, "127.0.0.1", SXE_LOCAL_PORT(listener)) == SXE_RETURN_OK, "Failed to sxe_connect()");
 
-    is_eq(tap_ev_identifier(event = test_tap_ev_queue_shift_wait(q_client, 2)), "test_event_connected", "got client connected event");
-    is_eq(tap_ev_identifier(event = test_tap_ev_queue_shift_wait(q_server, 2)), "test_event_connected", "got server connected event");
-    server = SXE_CAST(SXE *, tap_ev_arg(event, "this"));
+        event = test_tap_ev_queue_shift_wait(q_client, 2);
+        is_eq(tap_ev_identifier(event), "test_event_connected",                             "got client connected event");
+
+        event = test_tap_ev_queue_shift_wait(q_server, 2);
+        is_eq(tap_ev_identifier(event), "test_event_connected",                             "got server connected event");
+
+        /* Save the 'server' object for later tests */
+        server = SXE_CAST(SXE *, tap_ev_arg(event, "this"));
+    }
 
     /* client -> server: "HELO" */
-    result = sxe_send(client, "HELO", 4, test_event_sent);
-    if (result == SXE_RETURN_IN_PROGRESS) {
-        is_eq(tap_ev_identifier(event = test_tap_ev_queue_shift_wait(q_client, 2)), "test_event_sent", "got client sent event");
-    }
-    else {
-        skip(1, "client sent immediately - no need to wait for sent event");
+    {
+        result = sxe_send(client, "HELO", 4, test_event_sent);
+        if (result == SXE_RETURN_IN_PROGRESS) {
+            event = test_tap_ev_queue_shift_wait(q_client, 2);
+            is_eq(tap_ev_identifier(event), "test_event_sent",                              "got client sent event");
+        }
+        else {
+            skip(1, "client sent immediately - no need to wait for sent event");
+        }
+
+        event = test_tap_ev_queue_shift_wait(q_server, 2);
+        is_eq(tap_ev_identifier(event), "test_event_read",                                  "got server read event");
+        is(SXE_CAST(int, tap_ev_arg(event, "length")), 4,                                   "read a four byte value");
+        is(SXE_BUF_USED(server),     4,                                                     "four bytes in buffer");
+        is_strncmp(SXE_BUF(server), "HELO", 4,                                              "'HELO' in buffer");
     }
 
-    is_eq(tap_ev_identifier(event = test_tap_ev_queue_shift_wait(q_server, 2)), "test_event_read", "got server read event");
-    is(SXE_CAST(int, tap_ev_arg(event, "length")), 4,                                   "read a four byte value");
-    is(SXE_BUF_USED(server),     4,                                                     "four bytes in buffer");
-    is_strncmp(SXE_BUF(server), "HELO", 4,                                              "'HELO' in buffer");
+    /* server -> client: "HITHERE" */
+    {
+        result = sxe_send(server, "HITHERE", 7, test_event_sent);
+        if (result == SXE_RETURN_IN_PROGRESS) {
+            event = test_tap_ev_queue_shift_wait(q_server, 2);
+            is_eq(tap_ev_identifier(event), "test_event_sent",                              "got server sent event");
+        }
+        else {
+            skip(1, "server sent immediately - no need to wait for sent event");
+        }
 
-    /* server -> client: "GOODBYE" */
-    result = sxe_send(server, "GOODBYE", 7, test_event_sent);
-    if (result == SXE_RETURN_IN_PROGRESS) {
-        is_eq(tap_ev_identifier(event = test_tap_ev_queue_shift_wait(q_server, 2)), "test_event_sent", "got server sent event");
+        event = test_tap_ev_queue_shift_wait(q_client, 2);
+        is_eq(tap_ev_identifier(event), "test_event_read",                                  "got client read event");
+        is(tap_ev_arg(event, "this"), client,                                               "on the client");
+        is(SXE_CAST(int, tap_ev_arg(event, "length")), 7,                                   "read a 7 byte value");
+        is(SXE_BUF_USED(client),     7,                                                     "seven bytes in buffer");
+        is_eq(SXE_BUF(client), "HITHERE",                                                   "'HITHERE' in client buffer");
     }
-    else {
-        skip(1, "server sent immediately - no need to wait for sent event");
-    }
-    is_eq(tap_ev_identifier(event = test_tap_ev_queue_shift_wait(q_client, 2)), "test_event_read", "got client read event");
-    is(tap_ev_arg(event, "this"), client,                                           "on the client");
-    is(SXE_CAST(int, tap_ev_arg(event, "length")), 7,                               "read a 7 byte value");
-    is(SXE_BUF_USED(client),     7,                                                 "seven bytes in buffer");
-    is_eq(SXE_BUF(client), "GOODBYE",                                   "'GOODBYE' in client buffer");
 
-    sxe_close(client);
-    is_eq(tap_ev_identifier(event = test_tap_ev_queue_shift_wait(q_server, 2)), "test_event_close",      "got server close event");
-    is(tap_ev_arg(event, "this"), server,                                            "on the server");
+    /* Close the client, and ensure the server gets a close event. Note that
+     * the client doesn't get a close event: SXE never generates close events
+     * for explicit calls to sxe_close(), and SSL is no different. */
+    {
+        sxe_close(client);
+
+        event = test_tap_ev_queue_shift_wait(q_server, 2);
+        is_eq(tap_ev_identifier(event), "test_event_close",                                 "got server close event");
+        is(tap_ev_arg(event, "this"), server,                                               "on the server");
+    }
 
     is(sxe_fini(), SXE_RETURN_OK, "finished with sxe");
 
     return exit_status();
 }
+
+/* vim: set expandtab list sw=4 sts=4 listchars=tab\:^.,trail\:@: */
